fc82309c14292551d0131aed1e54d31ec52d4931
leossm-creator <ssangminleo@gmail.com>
2022-10-21 18:45:50 +1100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app id="screen">
+    <TopToolBar></TopToolBar>
+
+    <v-main>
+      <LandingScreen />
+    </v-main>
+
+    <BottomNav></BottomNav>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopToolBar from "./components/TopToolBar.vue";
+import BottomNav from "./components/BottomNav.vue";
 
 export default {
-  name: 'App',
+  name: "App",
+
   components: {
-    HelloWorld
-  }
-}
+    TopToolBar,
+    BottomNav,
+  },
+
+  data: () => ({
+
+  }),
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  #screen {
+    height: 100%;
+    max-height: 100%;
+  }
 </style>
---
+++ src/components/BottomNav.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <v-bottom-navigation
+      :value="active"
+      color="teal"
+      grow
+    >
+      <v-btn height="100%" value="wallet">
+        <span>Wallet</span>
+
+        <v-icon>mdi-wallet-outline</v-icon>
+      </v-btn>
+
+      <v-btn height="100%" value="home">
+        <span>Home</span>
+
+        <v-icon>mdi-home-outline</v-icon>
+      </v-btn>
+
+      <v-btn height="100%" value="progress">
+        <span>Progress</span>
+
+        <v-icon>mdi-progress-clock</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BottomNav",
+
+  components: {
+  },
+
+  data: () => ({
+    active: "home"
+  }),
+  computed: {
+  },
+};
+</script>
